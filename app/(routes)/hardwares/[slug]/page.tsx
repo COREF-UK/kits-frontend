49f6e58af38f948d2ea16df6dfd2cfbbfe189512
Josh Pollard <124677969+josh-thales@users.noreply.github.com>
2023-11-07 10:40:52 +0000

    added hardware pages

---
+++ app/(routes)/hardwares/[slug]/page.tsx
@@ -0,0 +1,14 @@
+"use-client";
+import SingleEntity from "@/app/_components/singleEntity";
+
+export const runtime = 'edge';
+
+export default function Theme({ params }: { params: { slug: string } }) {
+  return (
+    <SingleEntity
+      path="hardwares["
+      slug={params.slug}
+      displayRelations={[]}
+    ></SingleEntity>
+  );
+}
---
+++ app/(routes)/hardwares/page.tsx
@@ -0,0 +1,6 @@
+"use-client";
+import MultipleEntities from "@/app/_components/multipleEntities";
+
+export default function Themes() {
+  return <MultipleEntities path="hardwares" title="Hardwares"></MultipleEntities>;
+}
